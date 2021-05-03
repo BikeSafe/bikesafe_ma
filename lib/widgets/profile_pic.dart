5288bc1338d8ff4c51d47b62926ce8fffff56db3
Guiselle Tatiana Zambrano Penagos <gtzambranop@unal.edu.co>
2021-05-03 16:48:30 -0500

    count:Profile-User data

--- lib/widgets/profile_pic.dart
+++ lib/widgets/profile_pic.dart
@@ -4,33 +4,42 @@ import 'package:flutter/material.dart';
 
 class ProfilePic extends StatelessWidget {
   final String urlImage;
+  final String myText;
   final double rad;
   final double border;
+  final double margin;
 
-  ProfilePic({this.urlImage = "", this.rad = 100, this.border = 5});
+  ProfilePic(
+      {this.urlImage = "../../assets/images/profile_default.webp",
+      this.rad = 100,
+      this.border = 5,
+      this.myText = "DF",
+      this.margin = 20});
   Widget build(BuildContext context) {
-    return CircularProfileAvatar(
-      urlImage, //sets image path, it should be a URL string. default value is empty string, if path is empty it will display only initials
-      radius: rad, // sets radius, default 50.0
-      backgroundColor:
-          Colors.transparent, // sets background color, default Colors.white
-      borderWidth: border, // sets border, default 0.0
-      initialsText: Text(
-        "AD",
-        style: TextStyle(fontSize: 40, color: Colors.white),
-      ), // sets initials text, set your own style, default Text('')
-      borderColor:
-          DeepBlueColorScheme.gray, // sets border color, default Colors.white
-      elevation:
-          5.0, // sets elevation (shadow of the profile picture), default value is 0.0
-      foregroundColor: DeepBlueColorScheme.gray.withOpacity(
-          0.5), //sets foreground colour, it works if showInitialTextAbovePicture = true , default Colors.transparent
-      cacheImage: true, // allow widget to cache image against provided url
-      onTap: () {
-        print('adil');
-      }, // sets on tap
-      showInitialTextAbovePicture:
-          true, // setting it true will show initials text above profile picture, default false
-    );
+    return Container(
+        margin: EdgeInsets.fromLTRB(margin, margin, margin, margin),
+        child: CircularProfileAvatar(
+          urlImage, //sets image path, it should be a URL string. default value is empty string, if path is empty it will display only initials
+          radius: rad, // sets radius, default 50.0
+          backgroundColor:
+              Colors.transparent, // sets background color, default Colors.white
+          borderWidth: border, // sets border, default 0.0
+          initialsText: Text(
+            myText,
+            style: TextStyle(fontSize: 40, color: Colors.white),
+          ), // sets initials text, set your own style, default Text('')
+          borderColor: DeepBlueColorScheme
+              .gray, // sets border color, default Colors.white
+          elevation:
+              5.0, // sets elevation (shadow of the profile picture), default value is 0.0
+          foregroundColor: DeepBlueColorScheme.gray.withOpacity(
+              0.5), //sets foreground colour, it works if showInitialTextAbovePicture = true , default Colors.transparent
+          cacheImage: true, // allow widget to cache image against provided url
+          onTap: () {
+            print('adil');
+          }, // sets on tap
+          showInitialTextAbovePicture:
+              true, // setting it true will show initials text above profile picture, default false
+        ));
   }
 }
